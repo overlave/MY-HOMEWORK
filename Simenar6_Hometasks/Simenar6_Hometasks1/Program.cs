5d5a3d38961d79fae3c6d26fb643ed005a3d6ffa
overlave <dom142da@bk.ru>
2022-12-13 17:23:09 +0300

    6 hometasks

---
+++ Simenar6_Hometasks/Simenar6_Hometasks1/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear ();
+int[] CreateArray(int size)
+{
+    int[] array = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите элемент массива с индексом {i}: ");
+        array[i] = Convert.ToInt32 (Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+int SearchZero(int[] array)
+{
+    int summ = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) 
+        summ = summ + 1;
+    }
+    return summ;
+}
+
+Console.Write("Введите колличество элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+    if (size < 1)
+Console.WriteLine("Элементов массива не может быть меньше одного");
+    else
+{
+int[] arr = CreateArray(size);
+PrintArray(arr);
+int sum = SearchZero(arr);
+Console.WriteLine($"->{sum}");
+}
+ 
---
+++ Simenar6_Hometasks/Simenar6_Hometasks2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+//  y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Clear ();
+
+Console.WriteLine("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = (b2 - b1)/(k1-k2);
+double y = k1 * x + b1;
+
+    if (k1 == k2)
+    Console.WriteLine("Прямые параллельны и не пересекаются ");
+
+    else
+    Console.WriteLine($"b1 = {b1}, {b2} = 2, k1 = {k1}, k2 = {k2} -> ({x} ; {y})");
