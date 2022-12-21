32e9d168624db99d70d066632b031a75158ad663
overlave <dom142da@bk.ru>
2022-12-21 16:21:17 +0300

    9 hometasks

---
+++ Simenar9_Hometasks/Simenar9_Hometasks1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Clear ();
+
+int SumNumbers(int firstNum, int secondNum)
+{
+    if (firstNum == secondNum)
+    {
+        return firstNum;
+    }
+    if (firstNum > secondNum)
+    {
+        return secondNum + SumNumbers(firstNum, secondNum + 1);
+    }
+    return firstNum + SumNumbers(firstNum + 1, secondNum);
+}
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine(SumNumbers(firstNumber, secondNumber));
---
+++ Simenar9_Hometasks/Simenar9_Hometasks2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+Console.Clear ();
+
+int Akkerman(int firstNum, int secondNum)
+{
+    if (firstNum == 0)
+    {
+        return secondNum + 1;
+
+    }
+    else if ((firstNum != 0) && (secondNum == 0))
+    {
+        return Akkerman(firstNum - 1, 1);
+    }
+    return Akkerman(firstNum - 1, Akkerman(firstNum, secondNum - 1));
+}
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine(Akkerman(firstNumber, secondNumber));
